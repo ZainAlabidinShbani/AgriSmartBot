c461c67ec1cffc08b50fc36f00a2dbdb3be94302
ZainAlabidinShbani <zain.alabidin.shbani@gmail.com>
2025-10-05 16:57:39 +0300

    Update Weather

--- weather.py
+++ weather.py
@@ -19,7 +19,7 @@ def fetch_weather_by_coords(lat, lon):
         return "⚠️ خطأ في الاتصال بموقع الطقس."
 
 def fetch_forecast_by_coords(lat, lon, days=3):
-    url = f"https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude=minutely,hourly,alerts,current&appid={OPENWEATHER_API_KEY}&units=metric&lang=ar"
+    url = f"https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude=minutely,hourly,alerts,current&appid={OPENWEATHER_API_KEY}&units=metric&lang=ar"
     try:
         r = requests.get(url)
         data = r.json()
